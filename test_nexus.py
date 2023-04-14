6edac613c6570d28dc09470019d6a657595da484
Jonas Petersson <jonas.petersson@ess.eu>
2023-04-14 08:06:01 +0200

    adding passed metric

--- test_nexus.py
+++ test_nexus.py
@@ -3,7 +3,8 @@ from graphite_pusher import send_metric_to_graphite
 
 GRAPHITE_HOST = "10.100.211.201"
 GRAPHITE_PORT = 2003
-METRIC_NAME = "nexus_tester.failed_tests"
+FAILED_METRIC_NAME = "nexus_tester.failed_tests"
+PASSED_METRIC_NAME = "nexus_tester.passed_tests"
 
 def check_nexus_groups(file, errors):
     mandatory_groups = ['entry']
@@ -13,28 +14,34 @@ def check_nexus_groups(file, errors):
             return False
     return True
 
-def check_nxlog_group(nxlog_group, errors):
+def check_nxlog_group(nxlog_group, errors, passed):
     mandatory_fields = ['value', 'time']
     for field in mandatory_fields:
         if field not in nxlog_group:
             errors.append(f"Error: NXlog group '{nxlog_group.name}' must have '{field}' field.")
             continue
+        else:
+            passed.append(f"Passed: NXlog group '{nxlog_group.name}' has '{field}' field.")
 
         if 'units' not in nxlog_group[field].attrs:
             errors.append(f"Error: '{field}' field in NXlog group '{nxlog_group.name}' must have 'units' attribute.")
+        else:
+            passed.append(f"Passed: '{field}' field in NXlog group '{nxlog_group.name}' has 'units' attribute.")
 
         if len(nxlog_group[field]) < 1:
             errors.append(f"Error: NXlog group '{nxlog_group.name}' must have '{field}' field with a minimum length of 1.")
+        else:
+            passed.append(f"Passed: NXlog group '{nxlog_group.name}' has '{field}' field with a minimum length of 1.")
 
-def recursive_check(group, errors):
+def recursive_check(group, errors, passed):
     if group.attrs.get('NX_class') == 'NXlog':
-        check_nxlog_group(group, errors)
+        check_nxlog_group(group, errors, passed)
 
     for item in group.values():
         if isinstance(item, h5py.Group):
-            recursive_check(item, errors)
+            recursive_check(item, errors, passed)
 
-def check_entry_group(entry_group, errors):
+def check_entry_group(entry_group, errors, passed):
     mandatory_attributes = ['NX_class']
     mandatory_fields = ['title', 'start_time', 'end_time', 'definition']
 
@@ -42,30 +49,42 @@ def check_entry_group(entry_group, errors):
         if attr not in entry_group.attrs:
             errors.append(f"Error: Mandatory attribute '{attr}' not found in entry group.")
             return
+        else:
+            passed.append(f"Passed: Mandatory attribute '{attr}' found in entry group.")
 
     if entry_group.attrs['NX_class'] != 'NXentry':
         errors.append("Error: Entry group NX_class should be 'NXentry'.")
+    else:
+        passed.append("Passed: Entry group NX_class is 'NXentry'.")
 
     for field in mandatory_fields:
         if field not in entry_group:
             errors.append(f"Error: Mandatory field '{field}' not found in entry group.")
+        else:
+            passed.append(f"Passed: Mandatory field '{field}' found in entry group.")
 
-    recursive_check(entry_group, errors)
+    recursive_check(entry_group, errors, passed)
 
 def check_nexus_file(file_path):
     errors = []
+    passed = []
     with h5py.File(file_path, 'r') as file:
         check_nexus_groups(file, errors)
+        if not errors:
+            passed.append("Mandatory group 'entry' found.")
         entry_group = file.get('entry')
         if entry_group:
-            check_entry_group(entry_group, errors)
+            check_entry_group(entry_group, errors, passed)
 
     if errors:
-        send_metric_to_graphite(GRAPHITE_HOST, GRAPHITE_PORT, METRIC_NAME, len(errors))
+        send_metric_to_graphite(GRAPHITE_HOST, GRAPHITE_PORT, FAILED_METRIC_NAME, len(errors))
+        send_metric_to_graphite(GRAPHITE_HOST, GRAPHITE_PORT, PASSED_METRIC_NAME, len(passed))
         print(f"The following {len(errors)} errors were found:")
         for error in errors:
             print(f" - {error}")
     else:
+        send_metric_to_graphite(GRAPHITE_HOST, GRAPHITE_PORT, FAILED_METRIC_NAME, 0)
+        send_metric_to_graphite(GRAPHITE_HOST, GRAPHITE_PORT, PASSED_METRIC_NAME, len(passed))
         print("File adheres to NeXus standards.")
 
 if __name__ == '__main__':
