10db226f62a69281dc195f094481e345ba5dd6c3
Jonas Petersson <jonas.petersson@ess.eu>
2023-04-14 10:19:41 +0200

    trying graylog

---
+++ graylog_pusher.py
@@ -0,0 +1,40 @@
+import json
+import socket
+import time
+
+GRAYLOG_HOST = "10.100.211.187"
+GRAYLOG_PORT = 12201
+
+
+def create_gelf_message(message, level, additional_fields):
+    gelf_message = {
+        "version": "1.1",
+        "host": socket.gethostname(),
+        "short_message": message,
+        "full_message": message,
+        "timestamp": time.time(),
+        "level": level,  # Error level
+        "_facility": "NeXus File Tester",
+    }
+
+    for key, value in additional_fields.items():
+        gelf_message[f"_{key}"] = value
+
+    return json.dumps(gelf_message)
+
+
+def send_message_to_graylog(message, level, additional_fields=None):
+    if additional_fields is None:
+        additional_fields = {}
+
+    gelf_message = create_gelf_message(message, level, additional_fields)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((GRAYLOG_HOST, GRAYLOG_PORT))
+        sock.sendall(gelf_message.encode() + b"\x00")
+
+
+if __name__ == "__main__":
+    error_message = "An error occurred while processing a NeXus file."
+    additional_fields = {"filename": "example.nxs"}
+    send_message_to_graylog(error_message, 3, additional_fields)
--- test_nexus.py
+++ test_nexus.py
@@ -1,5 +1,6 @@
 import h5py
 from graphite_pusher import GraphiteQueue
+from graylog_pusher import send_message_to_graylog
 
 GRAPHITE_HOST = "10.100.211.201"
 GRAPHITE_PORT = 2003
@@ -8,32 +9,45 @@ PASSED_METRIC_NAME = "nexus_tester.passed_tests"
 
 graphite_queue = GraphiteQueue(GRAPHITE_HOST, GRAPHITE_PORT)
 
-def check_nexus_groups(file, errors):
+
+def register_error(error_message, error_list):
+    error_list.append(error_message)
+    send_message_to_graylog(error_message, 3)
+
+
+def register_pass(pass_message, pass_list):
+    pass_list.append(pass_message)
+    send_message_to_graylog(pass_message, 2)
+
+
+def check_nexus_groups(file, errors, passed):
     mandatory_groups = ['entry']
     for group in mandatory_groups:
         if group not in file:
-            errors.append(f"Error: Mandatory group '{group}' not found.")
+            register_error(f"Error: Mandatory group '{group}' not found.", errors)
             return False
+        else:
+            register_pass(f"Passed: Mandatory group '{group}' found.", passed)
     return True
 
 def check_nxlog_group(nxlog_group, errors, passed):
     mandatory_fields = ['value', 'time']
     for field in mandatory_fields:
         if field not in nxlog_group:
-            errors.append(f"Error: NXlog group '{nxlog_group.name}' must have '{field}' field.")
+            register_error(f"Error: NXlog group '{nxlog_group.name}' must have '{field}' field.", errors)
             continue
         else:
-            passed.append(f"Passed: NXlog group '{nxlog_group.name}' has '{field}' field.")
+            register_pass(f"Passed: NXlog group '{nxlog_group.name}' has '{field}' field.", passed)
 
         if 'units' not in nxlog_group[field].attrs:
-            errors.append(f"Error: '{field}' field in NXlog group '{nxlog_group.name}' must have 'units' attribute.")
+            register_error(f"Error: '{field}' field in NXlog group '{nxlog_group.name}' must have 'units' attribute.", errors)
         else:
-            passed.append(f"Passed: '{field}' field in NXlog group '{nxlog_group.name}' has 'units' attribute.")
+            register_pass(f"Passed: '{field}' field in NXlog group '{nxlog_group.name}' has 'units' attribute.", passed)
 
         if len(nxlog_group[field]) < 1:
-            errors.append(f"Error: NXlog group '{nxlog_group.name}' must have '{field}' field with a minimum length of 1.")
+            register_error(f"Error: NXlog group '{nxlog_group.name}' must have '{field}' field with a minimum length of 1.", errors)
         else:
-            passed.append(f"Passed: NXlog group '{nxlog_group.name}' has '{field}' field with a minimum length of 1.")
+            register_pass(f"Passed: NXlog group '{nxlog_group.name}' has '{field}' field with a minimum length of 1.", passed)
 
 def recursive_check(group, errors, passed):
     if group.attrs.get('NX_class') == 'NXlog':
@@ -49,21 +63,21 @@ def check_entry_group(entry_group, errors, passed):
 
     for attr in mandatory_attributes:
         if attr not in entry_group.attrs:
-            errors.append(f"Error: Mandatory attribute '{attr}' not found in entry group.")
+            register_error(f"Error: Mandatory attribute '{attr}' not found in entry group.", errors)
             return
         else:
-            passed.append(f"Passed: Mandatory attribute '{attr}' found in entry group.")
+            register_pass(f"Passed: Mandatory attribute '{attr}' found in entry group.", passed)
 
     if entry_group.attrs['NX_class'] != 'NXentry':
-        errors.append("Error: Entry group NX_class should be 'NXentry'.")
+        register_error("Error: Entry group NX_class should be 'NXentry'.", errors)
     else:
-        passed.append("Passed: Entry group NX_class is 'NXentry'.")
+        register_pass("Passed: Entry group NX_class is 'NXentry'.", passed)
 
     for field in mandatory_fields:
         if field not in entry_group:
-            errors.append(f"Error: Mandatory field '{field}' not found in entry group.")
+            register_error(f"Error: Mandatory field '{field}' not found in entry group.", errors)
         else:
-            passed.append(f"Passed: Mandatory field '{field}' found in entry group.")
+            register_pass(f"Passed: Mandatory field '{field}' found in entry group.", passed)
 
     recursive_check(entry_group, errors, passed)
 
@@ -71,9 +85,9 @@ def check_nexus_file(file_path):
     errors = []
     passed = []
     with h5py.File(file_path, 'r') as file:
-        check_nexus_groups(file, errors)
+        check_nexus_groups(file, errors, passed)
         if not errors:
-            passed.append("Mandatory group 'entry' found.")
+            register_pass("Mandatory group 'entry' found.", errors)
         entry_group = file.get('entry')
         if entry_group:
             check_entry_group(entry_group, errors, passed)
@@ -87,7 +101,7 @@ def check_nexus_file(file_path):
     else:
         graphite_queue.add_metric(FAILED_METRIC_NAME, 0)
         graphite_queue.add_metric(PASSED_METRIC_NAME, len(passed))
-        print("File adheres to NeXus standards.")
+        print("File adheres to NeXus standards.", errors)
 
 if __name__ == '__main__':
     nexus_file_path = '/home/jonas/code/motion_test/038243_00010245.hdf'
